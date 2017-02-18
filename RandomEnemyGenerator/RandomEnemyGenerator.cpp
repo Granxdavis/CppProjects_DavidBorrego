095d273e9a9b93f64d5146074fb78d033d60a37f
David <davidborregolopez@enti.cat>
2017-02-18 12:40:44 +0100

    Version2

--- RandomEnemyGenerator/RandomEnemyGenerator.cpp
+++ RandomEnemyGenerator/RandomEnemyGenerator.cpp
@@ -2,12 +2,9 @@
 #include <iostream>
 using namespace std;
 
-int jose = 3;
+
 enum EnemyType {Zombie, Vampire, Ghost, Witch};
 struct Enemy {	EnemyType type; 
 				string name; 
 				int health;
 				};
-void main(){
-	printf("number is \n",jose);
-}
